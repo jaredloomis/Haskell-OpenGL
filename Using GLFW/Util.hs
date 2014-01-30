227deff4c7cb36d784e7d481ae4bdcad7b465647
fiendfan1 <fiendfan1@yahoo.com>
2014-01-29 20:28:04 -0800

    Removed old directory

--- Using GLFW/Util.hs
+++
@@ -1,7 +0,0 @@
-module Util where
-
-import Data.Time.Clock (DiffTime, getCurrentTime, utctDayTime)
-import Control.Monad (liftM)
-
-systemTime :: IO DiffTime
-systemTime = liftM utctDayTime getCurrentTime
